1d9765aed703d940957b9a7bfd36014a1f9f166c
thriambakg <thriambak.g@gmail.com>
2025-07-20 16:34:06 -0500

    removing bucket

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,19 @@
-variable "project_name" {
-  description = "Name of the project"
-  type        = string
-}
+# variable "project_name" {
+#   description = "Name of the project"
+#   type        = string
+# }
 
-variable "test_root_bucket_name" {
-  description = "Test bucket identifier"
-  type        = string
-}
+# variable "test_root_bucket_name" {
+#   description = "Test bucket identifier"
+#   type        = string
+# }
 
-variable "environment" {
-  description = "Environment name (staging or production)"
-  type        = string
-}
+# variable "environment" {
+#   description = "Environment name (staging or production)"
+#   type        = string
+# }
 
-variable "bucketname" {
-  description = "Name of the S3 bucket"
-  type        = string
-}
+# variable "bucketname" {
+#   description = "Name of the S3 bucket"
+#   type        = string
+# }
