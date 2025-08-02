13ce199ef9dc8f8503bb9deb27af4768fb1c6941
thriambakg <thriambak.g@gmail.com>
2025-08-02 13:10:32 -0500

    deploying frontend

--- frontend/app/components/AmplifyClientConfig.tsx
+++ frontend/app/components/AmplifyClientConfig.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect } from 'react';
-import { configureAmplify } from '@/lib/amplifyConfig';
+import { configureAmplify } from '../lib/amplifyConfig';
 
 export default function AmplifyClientConfig({ children }: { children: React.ReactNode }) {
   useEffect(() => {
