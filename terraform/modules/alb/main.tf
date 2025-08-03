c25fc419fc7c3c3dbee51d175a62e13a90b558a3
thriambakg <thriambak.g@gmail.com>
2025-08-03 16:52:12 -0500

    prod push

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -462,7 +462,7 @@ resource "aws_wafv2_web_acl_logging_configuration" "main" {
   count        = var.enable_waf_logging ? 1 : 0
   resource_arn = aws_wafv2_web_acl.main.arn
   log_destination_configs = [
-    "arn:aws:logs:us-east-1:676206904242:log-group:/aws/wafv2/${var.project_name}-${var.environment}"
+    "${aws_cloudwatch_log_group.waf[0].arn}:*"
   ]
 
   depends_on = [
