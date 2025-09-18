f442dd90e1a7c061052dc6a1f25680e7d274d3ce
thriambakg <thriambak.g@gmail.com>
2025-09-17 22:13:31 -0500

    api changes

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -86,4 +86,28 @@ resource "aws_ecr_repository_policy" "frontend" {
   })
 }
 
+# ECR Repository policy for chat agent Lambda access
+resource "aws_ecr_repository_policy" "chat_agent" {
+  repository = aws_ecr_repository.chat_agent.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowLambdaPull"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetAuthorizationToken"
+        ]
+      }
+    ]
+  })
+}
+
 data "aws_caller_identity" "current" {}
