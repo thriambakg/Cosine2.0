faf2a8c3b6d4ac428793e3380af53a984736fd9a
Thri Giri <thriambak.g@gmail.com>
2025-12-21 00:46:36 -0600

    adding new LDA search integrations

--- terraform/main.tf
+++ terraform/main.tf
@@ -703,7 +703,7 @@ module "api_gateway" {
   tags = var.common_tags
 
   # Deployment trigger - increment this when you want to force a redeployment
-  deployment_trigger = "59" # Updated for remaining lambda-sqs migrations (all Lambdas)
+  deployment_trigger = "60" # Updated for remaining lambda-sqs migrations (all Lambdas)
 }
 
 # IAM Policy for Lambda functions to access Secrets Manager
