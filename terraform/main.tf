40ce370584a09eeb458a542e76903be11a2f2121
Thri Giri <thriambak.g@gmail.com>
2025-12-29 17:00:19 -0600

    build fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -775,7 +775,7 @@ module "api_gateway" {
   tags = var.common_tags
 
   # Deployment trigger - increment this when you want to force a redeployment
-  deployment_trigger = "72" # Updated for dashboard import endpoint
+  deployment_trigger = "73" # Updated for dashboard import endpoint
 }
 
 # IAM Policy for Lambda functions to access Secrets Manager
