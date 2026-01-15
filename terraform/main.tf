79854c111af556b57507b387237559c9b70596b6
Thri Giri <thriambak.g@gmail.com>
2026-01-14 20:19:26 -0600

    cors fixes

--- terraform/main.tf
+++ terraform/main.tf
@@ -890,7 +890,7 @@ module "api_gateway" {
   tags                 = var.common_tags
 
   # Deployment trigger - increment this when you want to force a redeployment
-  deployment_trigger = "84" # Updated for CORS fixes (dynamic origin headers)
+  deployment_trigger = "85" # Updated for CORS fixes (dynamic origin headers)
 }
 
 
